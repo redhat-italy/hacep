a75ec70fac40206f4f05576f1d5bf5689ee5697b
Ugo Landini <ugo.landini@gmail.com>
2016-07-06 14:21:26 +0200

    Updated script with correct jar name

--- hacep-examples/hacep-playground/scripts/startPerfClient.sh
+++ hacep-examples/hacep-playground/scripts/startPerfClient.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Script with preload (10 players, preload 20.000 fact for each one)
-java -Dconcurrent.players=10 -Ddelay.range=5 -Devent.interval=1 -Dtest.preload=true -Dtest.messages=20000 -Dbroker.host="localhost:61616" -Dbroker.security=true -Dbroker.usr=admin -Dbroker.pwd=admin -Dduration=480 -cp perf-client-1.0-SNAPSHOT.jar it.redhat.hacep.hacep.client.App
+java -Dconcurrent.players=10 -Ddelay.range=5 -Devent.interval=1 -Dtest.preload=true -Dtest.messages=20000 -Dbroker.host="localhost:61616" -Dbroker.security=true -Dbroker.usr=admin -Dbroker.pwd=admin -Dduration=480 -cp hacep-perf-client-1.0-SNAPSHOT.jar it.redhat.hacep.hacep.client.App
