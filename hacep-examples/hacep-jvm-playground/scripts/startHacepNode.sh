251d5bb7b29f0072aea95b701ca1269b29e837b9
Fabio Marinelli <f.marinelli@gmail.com>
2016-10-06 16:13:17 +0200

    modified script to launch HACEP with persistence enabled

--- hacep-examples/hacep-jvm-playground/scripts/startHacepNode.sh
+++ hacep-examples/hacep-jvm-playground/scripts/startHacepNode.sh
@@ -3,3 +3,7 @@
 # Local test with 2 nodes
 java -Xms4g -Xmx4g -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=localhost -Dgrid.buffer=5000 -Dqueue.url=tcp://localhost:61616 -Dqueue.security=true -Dqueue.usr=admin -Dqueue.pwd=admin -Dorg.apache.activemq.SERIALIZABLE_PACKAGES="*" -DnodeName=node1 -jar hacep-jvm-playground-1.0-SNAPSHOT.jar
 java -Xms4g -Xmx4g -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=localhost -Dgrid.buffer=5000 -Dqueue.url=tcp://localhost:61616 -Dqueue.security=true -Dqueue.usr=admin -Dqueue.pwd=admin -Dorg.apache.activemq.SERIALIZABLE_PACKAGES="*" -DnodeName=node2 -jar hacep-jvm-playground-1.0-SNAPSHOT.jar
+
+# Local test with persistence enabled
+java -Xms4g -Xmx4g -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=localhost -Dgrid.buffer=5000 -Dqueue.url=tcp://localhost:61616 -Dqueue.security=true -Dqueue.usr=admin -Dqueue.pwd=admin -Dorg.apache.activemq.SERIALIZABLE_PACKAGES="*" -DnodeName=node1 -Dgrid.persistence.evictionSize=5 -Dgrid.persistence.location=$TMPDIR/node1 -Dgrid.persistence=true -jar hacep-jvm-playground-1.0-SNAPSHOT.jar
+java -Xms4g -Xmx4g -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=localhost -Dgrid.buffer=5000 -Dqueue.url=tcp://localhost:61616 -Dqueue.security=true -Dqueue.usr=admin -Dqueue.pwd=admin -Dorg.apache.activemq.SERIALIZABLE_PACKAGES="*" -DnodeName=node2 -Dgrid.persistence.evictionSize=5 -Dgrid.persistence.location=$TMPDIR/node1 -Dgrid.persistence=true -jar hacep-jvm-playground-1.0-SNAPSHOT.jar
