475241a430778be22f8374e2b4df1f2db0fff447
Ugo Landini <ugo.landini@gmail.com>
2017-05-10 22:33:03 +0200

    updated community bits and created different build scripts

---
+++ build-community.sh
@@ -0,0 +1,1 @@
+mvn -Pcommunity -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=localhost clean install
---
+++ build-eap6.sh
@@ -0,0 +1,1 @@
+mvn -s example-maven-settings/settings.xml -Psupported-GA-ee6 -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=localhost clean install
