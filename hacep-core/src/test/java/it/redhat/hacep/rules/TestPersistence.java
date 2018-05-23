12a3cd44f125637bae9c3b902b92d4b100e44426
Andrea Tarocchi <atarocch@redhat.com>
2018-05-23 22:02:33 +0200

    Modified HAKieSessionDeltaFact merge to take in account that passing null values is expected not only during remove.
    Related to #91

--- hacep-core/src/test/java/it/redhat/hacep/rules/TestPersistence.java
+++ hacep-core/src/test/java/it/redhat/hacep/rules/TestPersistence.java
@@ -26,6 +26,7 @@ import it.redhat.hacep.configuration.RulesManager;
 import it.redhat.hacep.model.Fact;
 import org.infinispan.Cache;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.cache.SingleFileStoreConfigurationBuilder;
 import org.infinispan.eviction.EvictionStrategy;
 import org.infinispan.eviction.EvictionType;
 import org.junit.Assert;
@@ -48,11 +49,12 @@ import java.util.concurrent.Executors;
 
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
 
 @RunWith(MockitoJUnitRunner.class)
-public class TestPassivation extends AbstractClusterTest {
+public class TestPersistence extends AbstractClusterTest {
 
-    private final static Logger logger = LoggerFactory.getLogger(TestPassivation.class);
+    private final static Logger logger = LoggerFactory.getLogger(TestPersistence.class);
 
     private final static ExecutorService executorService = Executors.newFixedThreadPool(4);
 
@@ -69,34 +71,50 @@ public class TestPassivation extends AbstractClusterTest {
     @Mock
     private Channel locksChannel;
 
-    private String passivationLocation;
+    private int nodeSelector = 1;
+    private String persistenceLocation;
+    private String persistenceLocation2;
+    private String persistenceLocation3;
 
     @Before
     public void createTemporaryLocationName() {
-        passivationLocation = "./target/" + UUID.randomUUID().toString();
+        nodeSelector = 1;
+        persistenceLocation = "./target/" + UUID.randomUUID().toString();
+        persistenceLocation2 = "./target/" + UUID.randomUUID().toString();
+        persistenceLocation3 = "./target/" + UUID.randomUUID().toString();
     }
 
     @Override
     public ConfigurationBuilder extendDefaultConfiguration(ConfigurationBuilder builder) {
-        builder
+
+        SingleFileStoreConfigurationBuilder sfcb = builder
                 .persistence()
                 .passivation(false)
                 .addSingleFileStore()
                 .shared(false)
                 .preload(true)
                 .fetchPersistentState(true)
-                .purgeOnStartup(false)
-                .location(passivationLocation)
-//                .async().enabled(true)
-//                .threadPoolSize(5)
-                .singleton().enabled(false)
+                .purgeOnStartup(false);
+
+        if( nodeSelector == 1 ){
+            sfcb.location(persistenceLocation);
+        } else if( nodeSelector == 2) {
+            sfcb.location(persistenceLocation2);
+        } else if( nodeSelector == 3) {
+            sfcb.location(persistenceLocation3);
+        } else {
+            throw new UnsupportedOperationException("Node id unknown");
+        }
+
+        sfcb.singleton().enabled(false)
                 .eviction()
                 .strategy(EvictionStrategy.LRU).type(EvictionType.COUNT).size(1024);
+
         return builder;
     }
 
     @Test
-    public void testPassivation() {
+    public void testPersistence() {
         System.setProperty("grid.buffer", "10");
 
         reset(additionsChannel, replayChannel, locksChannel);
@@ -204,12 +222,12 @@ public class TestPassivation extends AbstractClusterTest {
     }
 
     @Test
-    public void testMultiNodePassivation() {
+    public void testMultiNodePersistence() throws InterruptedException {
         System.setProperty("grid.buffer", "10");
 
         reset(additionsChannel, replayChannel, locksChannel);
 
-        logger.info("Start test serialized rules");
+        logger.info("Start test Multi Node Persistence");
 
         RulesConfigurationTestImpl rulesConfigurationTest = RulesConfigurationTestImpl.RulesTestBuilder.buildRulesWithRetract();
         rulesConfigurationTest.registerChannel("additions", additionsChannel, replayChannel);
@@ -218,109 +236,91 @@ public class TestPassivation extends AbstractClusterTest {
         RulesManager rulesManager = new RulesManager(rulesConfigurationTest);
         rulesManager.start(null, null, null);
 
-        Cache<String, Object> cache = startNodes(2, rulesManager).getCache(CACHE_NAME);
-        Cache<String, Object> cache2 = startNodes(2, rulesManager).getCache(CACHE_NAME);
-        Cache<String, Object> cache3 = startNodes(2, rulesManager).getCache(CACHE_NAME);
+        nodeSelector = 1;
+        Cache<String, Object> cache = startNodes(2, rulesManager, "A", persistenceLocation).getCache(CACHE_NAME);
+        nodeSelector = 2;
+        Cache<String, Object> cache2 = startNodes(2, rulesManager, "B", persistenceLocation2).getCache(CACHE_NAME);
+        nodeSelector = 3;
+        Cache<String, Object> cache3 = startNodes(2, rulesManager, "C", persistenceLocation3).getCache(CACHE_NAME);
 
         String key3 = "3";
         HAKieSession session3 = new HAKieSession(rulesManager, executorService);
-        session3.insert(generateFactTenSecondsAfter(3, 1L));
+        // LOCK inserted expires in 25 sec.
+        session3.insert(generateFactTenSecondsAfter(3, 10L));
         cache3.put(key3, session3);
 
         String key2 = "2";
         HAKieSession session2 = new HAKieSession(rulesManager, executorService);
-        session2.insert(generateFactTenSecondsAfter(2, 1L));
+        // LOCK inserted expires in 25 sec.
+        session2.insert(generateFactTenSecondsAfter(2, 10L));
         cache2.put(key2, session2);
 
         String key = "1";
         HAKieSession session1 = new HAKieSession(rulesManager, executorService);
-
-        session1.insert(generateFactTenSecondsAfter(1, 1L));
-        cache.put(key, session1);
-
-        session1.insert(generateFactTenSecondsAfter(1, 2L));
-        cache.put(key, session1);
-
-        session1.insert(generateFactTenSecondsAfter(1, 3L));
-        cache.put(key, session1);
-
-        session1.insert(generateFactTenSecondsAfter(1, 4L));
-        cache.put(key, session1);
-
-        session1.insert(generateFactTenSecondsAfter(1, 5L));
-        cache.put(key, session1);
-
-        session1.insert(generateFactTenSecondsAfter(1, 6L));
-        cache.put(key, session1);
-
-        session1.insert(generateFactTenSecondsAfter(1, 7L));
-        cache.put(key, session1);
-
-        session1.insert(generateFactTenSecondsAfter(1, 8L));
-        cache.put(key, session1);
-
-        session1.insert(generateFactTenSecondsAfter(1, 9L));
-        cache.put(key, session1);
-
-        session1.insert(generateFactTenSecondsAfter(1, 10L));
         // LOCK inserted expires in 25 sec.
-        cache.put(key, session1);
-
-
-        session1.insert(generateFactTenSecondsAfter(1, 1L));
-        cache.put(key, session1);
-
-        session1.insert(generateFactTenSecondsAfter(1, 2L));
-        cache.put(key, session1);
-
-        // 30 sec after - lock should be expired
-        session1.insert(generateFactTenSecondsAfter(1, 3L));
-        cache.put(key, session1);
-
         session1.insert(generateFactTenSecondsAfter(1, 10L));
-        // LOCK inserted expires in 25 sec.
         cache.put(key, session1);
+
+        InOrder order2 = Mockito.inOrder(additionsChannel, locksChannel);
+        order2.verify(locksChannel, times(3)).send(eq("INSERTED"));
+        order2.verifyNoMoreInteractions();
 
         stopNodes();
 
-//        Cache<String, Object> cacheDeserialized = startNodes(1, rulesManager).getCache(CACHE_NAME);
-//
-//        Object o = cacheDeserialized.get(key);
-//        Assert.assertTrue(o instanceof HAKieSerializedSession);
-//        HAKieSerializedSession haKieSerializedSession = (HAKieSerializedSession) o;
-//        HAKieSession sessionRebuilt = haKieSerializedSession.rebuild();
-//
-//        sessionRebuilt.insert(generateFactTenSecondsAfter(1, 0L));
-//        cacheDeserialized.put(key, sessionRebuilt);
-//
-//        sessionRebuilt.insert(generateFactTenSecondsAfter(1, 0L));
-//        cacheDeserialized.put(key, sessionRebuilt);
-//
-//        // 30 sec after - lock should be expired
-//        // And inserted again by this fact (expires in 25 sec)
-//        sessionRebuilt.insert(generateFactTenSecondsAfter(1, 10L));
-//        cacheDeserialized.put(key, sessionRebuilt);
-//
-//        InOrder order = Mockito.inOrder(additionsChannel, locksChannel);
-//        order.verify(additionsChannel).send(eq(1L));
-//        order.verify(additionsChannel).send(eq(2L));
-//        order.verify(additionsChannel).send(eq(3L));
-//        order.verify(additionsChannel).send(eq(4L));
-//        order.verify(additionsChannel).send(eq(5L));
-//        order.verify(additionsChannel).send(eq(6L));
-//        order.verify(additionsChannel).send(eq(7L));
-//        order.verify(additionsChannel).send(eq(8L));
-//        order.verify(additionsChannel).send(eq(9L));
-//        order.verify(locksChannel).send(eq("INSERTED"));
-//        order.verify(locksChannel).send(eq("REMOVED"));
-//        order.verify(additionsChannel).send(eq(3L));
-//        order.verify(locksChannel).send(eq("INSERTED"));
-//        order.verify(locksChannel).send(eq("REMOVED"));
-//        order.verify(locksChannel).send(eq("INSERTED"));
-//        order.verifyNoMoreInteractions();
-//
-//        logger.info("End test serialized rules");
-//        rulesManager.stop();
+        nodeSelector = 1;
+        Cache<String, Object> cacheDeserialized = startNodes(2, rulesManager, "A", persistenceLocation).getCache(CACHE_NAME);
+        nodeSelector = 2;
+        Cache<String, Object> cacheDeserialized2 = startNodes(2, rulesManager, "B", persistenceLocation2).getCache(CACHE_NAME);
+        nodeSelector = 3;
+        Cache<String, Object> cacheDeserialized3 = startNodes(2, rulesManager, "C", persistenceLocation3).getCache(CACHE_NAME);
+
+
+        checkKey(key, cacheDeserialized, cacheDeserialized2, cacheDeserialized3);
+        checkKey(key2, cacheDeserialized, cacheDeserialized2, cacheDeserialized3);
+        checkKey(key3, cacheDeserialized, cacheDeserialized2, cacheDeserialized3);
+
+
+        Mockito.verify( locksChannel, times(6) ).send(eq("INSERTED"));
+        Mockito.verify( locksChannel, times(3) ).send(eq("REMOVED"));
+        Mockito.verifyNoMoreInteractions(locksChannel);
+
+        logger.info("Multi Node Persistence");
+        rulesManager.stop();
+    }
+
+    private void checkKey(String key, Cache<String, Object> cacheDeserialized, Cache<String, Object> cacheDeserialized2, Cache<String, Object> cacheDeserialized3) {
+
+        logger.info("Checking key: "+key);
+
+        long keyLong = Long.parseLong(key);
+
+        Object o = cacheDeserialized.get(key);
+        //XXX: needed waiting for https://issues.jboss.org/browse/ISPN-9200
+        if( o == null){
+            o = cacheDeserialized2.get(key);
+        }
+        if( o == null){
+            o = cacheDeserialized3.get(key);
+        }
+        ///////////////////////////////////////////////////////////////////
+
+        Assert.assertTrue(o instanceof HAKieSerializedSession);
+        HAKieSerializedSession haKieSerializedSession = (HAKieSerializedSession) o;
+        HAKieSession sessionRebuilt = haKieSerializedSession.rebuild();
+
+        sessionRebuilt.insert(generateFactTenSecondsAfter(keyLong, 0L));
+        cacheDeserialized.put(key, sessionRebuilt);
+
+        sessionRebuilt.insert(generateFactTenSecondsAfter(keyLong, 0L));
+        cacheDeserialized.put(key, sessionRebuilt);
+
+        // 30 sec after - lock should be expired
+        sessionRebuilt.insert(generateFactTenSecondsAfter(keyLong, 0L));
+        cacheDeserialized.put(key, sessionRebuilt);
+
+        // And inserted again by this fact (expires in 25 sec)
+        sessionRebuilt.insert(generateFactTenSecondsAfter(keyLong, 10L));
+        cacheDeserialized.put(key, sessionRebuilt);
     }
 
     @Override
